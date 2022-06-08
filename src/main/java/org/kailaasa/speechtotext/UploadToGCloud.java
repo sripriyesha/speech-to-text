a54096f28c5cad1625eee67917fbae03dfe2da83
florentroques <68758690+sripriyesha@users.noreply.github.com>
2022-06-08 22:45:44 +0200

    Update UploadToGCloud.java

--- src/main/java/org/kailaasa/speechtotext/UploadToGCloud.java
+++ src/main/java/org/kailaasa/speechtotext/UploadToGCloud.java
@@ -1,53 +1,29 @@
 package org.kailaasa.speechtotext;
 
+import com.google.api.gax.paging.Page;
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
+import java.io.File;
+import java.io.FilenameFilter;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Stream;
 
 public class UploadToGCloud {
     public static void uploadObject(
-        String projectId,
+        Storage storage,
         String bucketName,
         String objectName,
         String filePath
-    ) throws IOException {
-        // The ID of your GCP project
-        // String projectId = "your-project-id";
-
-        // The ID of your GCS bucket
-        // String bucketName = "your-unique-bucket-name";
-
-        // The ID of your GCS object
-        // String objectName = "your-object-name";
-
-        // The path to your file to upload
-        // String filePath = "path/to/your/file"
-        
-        GoogleCredentials credentials = GoogleCredentials.fromStream(
-                new FileInputStream("speech-to-text-352619-e6ac4e047752.json")
-        ).createScoped(
-                Arrays.asList("https://www.googleapis.com/auth/cloud-platform")
-        );
-
-        Storage storage = StorageOptions
-                .newBuilder()
-                .setProjectId(projectId)
-                .setCredentials(credentials)
-                .build()
-                .getService()
-        ;
-        
+    ) throws IOException {        
         BlobId blobId = BlobId.of(bucketName, objectName);
         BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
         storage.create(blobInfo, Files.readAllBytes(Paths.get(filePath)));
@@ -57,23 +33,55 @@ public class UploadToGCloud {
     }
   
     public static void main(String... args) throws Exception {
-        try (Stream<Path> paths = Files.walk(Paths.get("./files"))) {
-            paths
-            .filter(Files::isRegularFile)
-            .filter(p -> p.getFileName().toString().endsWith(".flac"))
-            .forEach(p -> {
-                try {
-                    uploadObject(
-                        "speech-to-text",
-                        "speech-to-text-sharabheshwara-bucket1",
-                        p.getFileName().toString(),
-                        "./files/" + p.getFileName().toString()
-                    );
-                } catch (IOException ex) {
-                    Logger.getLogger(UploadToGCloud.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            });
-        } 
-    }
+        GoogleCredentials credentials = GoogleCredentials.fromStream(
+                new FileInputStream("speech-to-text-352619-e6ac4e047752.json")
+        ).createScoped(
+                Arrays.asList("https://www.googleapis.com/auth/cloud-platform")
+        );
 
+        Storage storage = StorageOptions
+                .newBuilder()
+                .setProjectId("speech-to-text")
+                .setCredentials(credentials)
+                .build()
+                .getService()
+        ;
+        
+        File dir = new File("./files");
+        File[] directoryListing = dir.listFiles(new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+                return name.toLowerCase().endsWith(".flac");
+            }
+        });
+        
+        for (File file : directoryListing) {
+            if (Files.exists(Paths.get("./subtitles/" + file.getName() + ".srt"))) {
+                System.out.println(file.getName() + " subtitles already retrieved");
+                continue;
+            }
+            
+            boolean isAlreadyUploaded = false;
+            
+            Page<Blob> blobs = storage.list("speech-to-text-sharabheshwara-bucket1");
+            
+            for (Blob blob : blobs.iterateAll()) {
+                boolean fileAlreadyExists = file.getName().equals(blob.getName());
+                
+                if (fileAlreadyExists) {
+                    System.out.println(file.getName() + " already uploaded");
+                    isAlreadyUploaded = true;
+                    break;
+                }
+            }
+            
+            if (!isAlreadyUploaded) {
+                uploadObject(
+                    storage,
+                    "speech-to-text-sharabheshwara-bucket1",
+                    file.getName(),
+                    "./files/" + file.getName()
+                );
+            }
+        }
+    }
 }
